2126a267b174b40ee9cd25d01a7ac3d53d457811
Russell <r@snds.gd>
2015-01-09 08:33:47 -0500

    fixes namespace issue

--- src/Memcached.php
+++ src/Memcached.php
@@ -16,15 +16,15 @@ class Memcached extends \Memcached
    {
       parent::__construct($persistentId);
       if (empty($this->getServerList())) {
-         $this->setOption(Memcached::OPT_PREFIX_KEY, 'sndsgd-');
-         $this->setOption(Memcached::OPT_RECV_TIMEOUT, 1000);
-         $this->setOption(Memcached::OPT_SEND_TIMEOUT, 1000);
-         $this->setOption(Memcached::OPT_TCP_NODELAY, true);
-         $this->setOption(Memcached::OPT_SERVER_FAILURE_LIMIT, 50);
-         $this->setOption(Memcached::OPT_CONNECT_TIMEOUT, 500);
-         $this->setOption(Memcached::OPT_RETRY_TIMEOUT, 300);
-         $this->setOption(Memcached::OPT_REMOVE_FAILED_SERVERS, true);
-         $this->setOption(Memcached::OPT_LIBKETAMA_COMPATIBLE, true);
+         $this->setOption(\Memcached::OPT_PREFIX_KEY, 'sndsgd-');
+         $this->setOption(\Memcached::OPT_RECV_TIMEOUT, 1000);
+         $this->setOption(\Memcached::OPT_SEND_TIMEOUT, 1000);
+         $this->setOption(\Memcached::OPT_TCP_NODELAY, true);
+         $this->setOption(\Memcached::OPT_SERVER_FAILURE_LIMIT, 50);
+         $this->setOption(\Memcached::OPT_CONNECT_TIMEOUT, 500);
+         $this->setOption(\Memcached::OPT_RETRY_TIMEOUT, 300);
+         $this->setOption(\Memcached::OPT_REMOVE_FAILED_SERVERS, true);
+         $this->setOption(\Memcached::OPT_LIBKETAMA_COMPATIBLE, true);
          $this->addServer('127.0.0.1', 11211, 1);
       }
    }
