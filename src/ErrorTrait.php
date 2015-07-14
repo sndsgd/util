68a5c3e42c5ad0853e51f217591c1ef2c2917123
Russell <r@snds.gd>
2015-07-14 18:06:42 -0400

    adds error trait

---
+++ src/ErrorTrait.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace sndsgd;
+
+
+trait ErrorTrait
+{
+   /**
+    * An error message
+    * 
+    * @var string|null
+    */
+   protected $error;
+
+   /**
+    * An detailed error message
+    * 
+    * @var string|null
+    */
+   protected $errorDetail;
+
+   /**
+    * Set an error message
+    *
+    * @param string $msg
+    */
+   protected function setError($msg)
+   {
+      $this->error = $msg;
+      if (($err = error_get_last()) !== null) {
+         $this->errorDetail = 
+            "'{$err['message']}' in {$err['file']} on line {$err['line']}";
+      }
+   }
+
+   /**
+    * Retreive the current error
+    * 
+    * @return string|null
+    */
+   public function getError()
+   {
+      return $this->error;
+   }
+
+   /**
+    * Retreive the current error detail
+    * 
+    * @return string|null
+    */
+   public function getErrorDetail()
+   {
+      return $this->errorDetail;
+   }
+
+   /**
+    * Reset the errors
+    */
+   public function clearError()
+   {
+      $this->error = $this->errorDetail = null;
+   }
+}
+
---
+++ tests/ErrorTraitTest.php
@@ -0,0 +1,45 @@
+<?php
+
+
+class TestErrorTrait
+{
+   use \sndsgd\ErrorTrait;
+
+   public function fail()
+   {
+      $this->error = "failed";
+      $this->errorDetail = "detail";
+      return false;
+   }
+
+   public function forceSetError()
+   {
+      @trigger_error("test", E_USER_NOTICE);
+      $this->setError("forced");
+   }
+}
+
+
+class ErrorTraitTest extends PHPUnit_Framework_TestCase
+{
+   public function test()
+   {
+      $test = new TestErrorTrait;
+      $this->assertNull($test->getError());
+      $this->assertNull($test->getErrorDetail());
+
+      $test->fail();
+      $this->assertEquals("failed", $test->getError());
+      $this->assertEquals("detail", $test->getErrorDetail());
+
+      $test->clearError();
+      $this->assertNull($test->getError());
+      $this->assertNull($test->getErrorDetail());
+
+      $test->forceSetError();
+      $this->assertEquals("forced", $test->getError());
+      $expect = "'test' in ".__FILE__." on line 17";
+      $this->assertEquals($expect, $test->getErrorDetail());
+   }
+}
+
