c1a28552429e53c0fb42a5a0619c8beefb59ad93
Russell <r@snds.gd>
2016-05-04 07:31:01 -0400

    add ArrayAbstract

---
+++ src/ArrayAbstract.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace sndsgd;
+
+/**
+ * An object powered array implemention
+ * Note: this class exists solely for creating typed arrays
+ */
+abstract class ArrayAbstract implements \ArrayAccess, \Countable, \IteratorAggregate
+{
+    /**
+     * The array values are stored here
+     *
+     * @var array
+     */
+    protected $values;
+
+    /**
+     * Whether the array may be altered after it is created
+     *
+     * @var bool
+     */
+    protected $isReadOnly;
+
+    /**
+     * @param array $values
+     * @param bool $isReadOnly
+     */
+    public function __construct(array $values, bool $isReadOnly = false)
+    {
+        $this->values = $values;
+        $this->isReadOnly = $isReadOnly;
+    }
+
+    /**
+     * @see http://php.net/manual/countable.count
+     * @return int
+     */
+    public function count()
+    {
+        return count($this->values);
+    }
+
+    /**
+     * @see http://php.net/manual/arrayaccess.offsetexists
+     * @param mixed $offset
+     * @return bool
+     */
+    public function offsetExists($offset)
+    {
+        return isset($this->values[$offset]);
+    }
+
+    /**
+     * @see http://php.net/manual/arrayaccess.offsetget
+     * @param mixed $offset
+     * @return mixed
+     */
+    public function offsetGet($offset)
+    {
+        return $this->offsetExists($offset) ? $this->values[$offset] : null;
+    }
+
+    /**
+     * @see http://php.net/manual/arrayaccess.offsetset
+     * @param mixed $offset
+     * @param mixed $value
+     * @throws \RuntimeException
+     */
+    public function offsetSet($offset, $value)
+    {
+        if ($this->isReadOnly) {
+            $class = get_class($this);
+            throw new \RuntimeException(
+                "failed to set array value; $class is read only"
+            );
+        } elseif ($offset === null) {
+            $this->values[] = $value;
+        } else {
+            $this->values[$offset] = $value;
+        }
+    }
+
+    /**
+     * @see http://php.net/manual/arrayaccess.offsetunset
+     * @param mixed $offset
+     * @throws \RuntimeException
+     */
+    public function offsetUnset($offset)
+    {
+        if ($this->isReadOnly) {
+            $class = get_class($this);
+            throw new \RuntimeException(
+                "failed to unset array value; $class is read only"
+            );
+        }
+        unset($this->values[$offset]);
+    }
+
+    /**
+     * @see http://php.net/manual/iteratoraggregate.getiterator
+     * @return \ArrayIterator
+     */
+    public function getIterator()
+    {
+        return new \ArrayIterator($this->values);
+    }
+}
---
+++ tests/ArrayAbstractTest.php
@@ -0,0 +1,146 @@
+<?php
+
+namespace sndsgd;
+
+class ArrayAbstractTest extends \PHPUnit_Framework_TestCase
+{
+    private function createAnonymousClass(array $values, $isReadOnly = false)
+    {
+        return new class($values, $isReadOnly) extends ArrayAbstract {};
+    }
+
+    /**
+     * @dataProvider providerOffsetExists
+     */
+    public function testOffsetExists($values, $offset, $expect)
+    {
+        $test = $this->createAnonymousClass($values);
+        $this->assertSame($expect, $test->offsetExists($offset));
+        $this->assertSame($expect, isset($test[$offset]));
+    }
+
+    public function providerOffsetExists()
+    {
+        return [
+            [[1,2], 0, true],
+            [[1,2], 1, true],
+            [[1,2], 2, false],
+            [["one" => 1, "two" => 2], "one", true],
+            [["one" => 1, "two" => 2], "three", false],
+        ];
+    }
+
+    /**
+     * @dataProvider providerOffsetGet
+     */
+    public function testOffsetGet($values, $offset, $expect)
+    {
+        $test = $this->createAnonymousClass($values);
+        $this->assertSame($expect, $test->offsetGet($offset));
+        $this->assertSame($expect, $test[$offset]);
+    }
+
+    public function providerOffsetGet()
+    {
+        return [
+            [[1,2], 0, 1],
+            [[1,2], 1, 2],
+            [[1,2], 2, null],
+            [["one" => 1, "two" => 2], "one", 1],
+        ];
+    }
+
+    /**
+     * @dataProvider providerOffsetSet
+     */
+    public function testOffsetSet($values, $offset)
+    {
+        if ($offset === null) {
+            $test = $this->createAnonymousClass($values);
+            $count = count($test);
+            $value = Str::random(10);
+            $test->offsetSet($offset, $value);
+            $this->assertCount($count + 1, $test);
+
+            $count = count($test);
+            $value = Str::random(10);
+            $test[] = $value;
+            $this->assertCount($count + 1, $test);
+        } else {
+            $test = $this->createAnonymousClass($values);
+            $value = Str::random(10);
+            $test->offsetSet($offset, $value);
+            $this->assertSame($value, $test[$offset]);
+
+            $test = $this->createAnonymousClass($values);
+            $value = Str::random(10);
+            $test[$offset] = $value;
+            $this->assertSame($value, $test[$offset]);
+        }
+    }
+
+    public function providerOffsetSet()
+    {
+        return [
+            [[], null],
+            [[1], 0],
+            [[1], 1],
+            [["one" => 1, "two" => 2], "one"],
+        ];
+    }
+
+    /**
+     * @expectedException \RuntimeException
+     */
+    public function testOffsetSetReadOnly()
+    {
+        $test = $this->createAnonymousClass([], true);
+        $test["test"] = "value";
+    }
+
+    /**
+     * @dataProvider providerOffsetUnset
+     */
+    public function testOffsetUnset($values, $offset)
+    {
+        $test = $this->createAnonymousClass([$offset => Str::random(10)]);
+        $this->assertTrue(isset($test[$offset]));
+        $test->offsetUnset($offset);
+        $this->assertFalse(isset($test[$offset]));
+
+        $test = $this->createAnonymousClass([$offset => Str::random(10)]);
+        $this->assertTrue(isset($test[$offset]));
+        unset($test[$offset]);
+        $this->assertFalse(isset($test[$offset]));
+    }
+
+    public function providerOffsetUnset()
+    {
+        return [
+            [[], 0],
+            [[1], 0],
+            [[1], 1],
+            [["one" => 1, "two" => 2], "one"],
+        ];
+    }
+
+    /**
+     * @expectedException \RuntimeException
+     */
+    public function testOffsetUnsetReadOnly()
+    {
+        $test = $this->createAnonymousClass(["test" => 1], true);
+        unset($test["test"]);
+    }
+
+    public function testGetIterator()
+    {
+        $values = [1, "two", 3.3, [4]];
+        $test = $this->createAnonymousClass($values);
+        $iterator = $test->getIterator();
+        $this->assertInstanceOf(\ArrayIterator::class, $iterator);
+        foreach ($test as $index => $value) {
+            $this->assertSame($value, $values[$index]);
+        }
+    }
+}
