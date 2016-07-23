f20748511e6b9c41a1b3318e27a4692634e76d78
Russell Barnhart <russell@tumblr.com>
2016-07-23 13:40:36 -0400

    update config for use as an instance

--- src/Config.php
+++ src/Config.php
@@ -2,99 +2,49 @@
 
 namespace sndsgd;
 
-/**
- * Config storage and retrieval
- */
 class Config
 {
     /**
-     * Config value storage
+     * Config values are stored here
      *
-     * @var array<string|integer,mixed>
+     * @var array<string,mixed>
      */
-    private static $values = [];
+    protected $values = [];
 
     /**
-     * Set initial config values
-     *
-     * @param array<string,mixed> $values
+     * @param array $values <string,mixed>
      */
-    public static function init(array $values = [])
+    public function __construct(array $values)
     {
-        self::$values = $values;
+        $this->values = $values;
     }
 
     /**
-     * Set one or more config values
+     * Retrieve a value, or a default value if it doesn't exist
      *
-     * @param string|array $key The unique identifier for the value
-     * @param number|string|null $value The value
-     */
-    public static function set($key, $value = null)
-    {
-        if (is_array($key)) {
-            self::$values = array_merge(self::$values, $key);
-        }
-        else {
-            self::$values[$key] = $value;
-        }
-    }
-
-    /**
-     * Get a config value
-     *
-     * @param string $key The unique key for the value to fetch
-     * @param string|null $default A value to return if the key does not exist
+     * @param string $key The key of the value to retrieve
+     * @param mixed $default The value to return if $key does not exist
      * @return mixed
      */
-    public static function get(string $key, $default = null)
+    public function get(string $key, $default = null)
     {
-        return (array_key_exists($key, self::$values))
-            ? self::$values[$key]
-            : $default;
+        return $this->values[$key] ?? $default;
     }
 
     /**
-     * Get a config value, and throw an exception if it doesn't exist
+     * Retrieve a value that must exist
      *
-     * @param string $key The unique key for the value to fetch
+     * @param string $key The key of the value to retrieve
      * @return mixed
-     * @throws \Exception if $key is not set
+     * @throws \RuntimeException If the key does not exist
      */
-    public static function getRequired(string $key)
+    public function getRequired(string $key)
     {
-        if (array_key_exists($key, self::$values)) {
-            return self::$values[$key];
+        if (!isset($this->values[$key])) {
+            throw new \RuntimeException(
+                "the required config value '$key' was not found"
+            );
         }
-        throw new \Exception("the required config value '$key' was not found");
-    }
-
-    /**
-     * Get multiple values with specific keys as array
-     *
-     * @param array<string,string> $map configName => returnName
-     * @return array<string,mixed> All values are found
-     * @return string An error message indicating a missing config value
-     */
-    public static function getAs(array $map)
-    {
-        $ret = [];
-        foreach ($map as $key => $retKey) {
-            if (!array_key_exists($key, self::$values)) {
-                return "the required config value '$key' was not found";
-            }
-            $ret[$retKey] = self::$values[$key];
-        }
-        return $ret;
-    }
-
-    /**
-     * Get the entire config array
-     *
-     * @return array<string,mixed>
-     */
-    public static function getAll(): array
-    {
-        return self::$values;
+        return $this->values[$key];
     }
 }
--- tests/ConfigTest.php
+++ tests/ConfigTest.php
@@ -1,87 +1,75 @@
 <?php
 
-use \sndsgd\Config;
+namespace sndsgd;
 
-
-class ConfigTest extends PHPUnit_Framework_TestCase
+/**
+ * @coversDefaultClass \sndsgd\Config
+ */
+class ConfigTest extends \PHPUnit_Framework_TestCase
 {
-    public static function setUpBeforeClass()
-    {
-        Config::init(self::$values['init']);
-    }
-
-    public static function tearDownAfterClass()
-    {
-        Config::init([]);
-    }
-
     protected static $values = [
-        'init' => [
-            'test.one' => 1,
-            'test.two' => 2,
-            'test.three' => 3,
-        ]
+        "string" => "testing",
+        "integer" => 42,
+        "float" => 4.2,
+        "array" => [
+            "qDERhNa1S0Pwa5GsmkzHZxDwv9DBAqzOLxCdQh3VuF",
+            "mKJcR2DSvcshn5K8SGlqxOnTRUOBmYj1eQD72gZbL4",
+        ],
     ];
 
-    public function testSet()
+    protected $config;
+
+    public function setUp()
     {
-        Config::set('test.set.one', 1);
-        Config::set([
-            'test.set.two' => 2,
-            'test.set.three' => 3
-        ]);
-
-        $this->assertEquals(1, Config::get('test.set.one'));
-        $this->assertEquals(2, Config::get('test.set.two'));
-        $this->assertEquals(3, Config::get('test.set.three'));
-        $this->assertNull(Config::get('undefined.key'));
-    }
-
-    public function testGet()
-    {
-        # set with Config::init()
-        $this->assertEquals(1, Config::get('test.one'));
-        $this->assertEquals(2, Config::get('test.two'));
-        $this->assertEquals(3, Config::get('test.three'));
-
-        # default value
-        $this->assertEquals('noval', Config::get('test.undefined', 'noval'));
-    }
-
-    public function testGetRequired()
-    {
-        $this->assertEquals(1, Config::getRequired('test.one'));
+        $this->config = new Config(static::$values);
     }
 
     /**
-     * @expectedException \Exception
+     * @covers ::__construct
      */
-    public function testGetRequiredException()
+    public function testConstructor()
     {
-        $value = Config::getRequired('test.undefined');
+        $config = new Config(static::$values);
+        $this->assertInstanceOf(Config::class, $config);
     }
 
-    public function testGetAs()
+    /**
+     * @covers ::get
+     * @dataProvider providerGet
+     */
+    public function testGet($key, $default, $expect)
     {
-        $map = [
-            'test.one' => 'one',
-            'test.two' => 'two'
-        ];
-
-        $values = Config::getAs($map);
-        $this->assertEquals(1, $values['one']);
-        $this->assertEquals(2, $values['two']);
-
-        $map = [
-            'test.undefined' => 'shrtval'
-        ];
-        $expect = "the required config value 'test.undefined' was not found";
-        $this->assertEquals($expect, Config::getAs($map));
+        $this->assertSame($expect, $this->config->get($key, $default));
     }
 
-    public function testGetAll()
+    public function providerGet()
     {
-        Config::init(self::$values['init']);
-        $this->assertEquals(self::$values['init'], Config::getAll());
+        $rand = \sndsgd\Str::random(42);
+        return [
+            ["string", "", static::$values["string"]],
+            ["integer", 0, static::$values["integer"]],
+            ["array", [], static::$values["array"]],
+            ["missing", $rand, $rand],
+        ];
+    }
+
+    /**
+     * @dataProvider providerGetRequired
+     * @covers ::getRequired
+     */
+    public function testGetRequired($key, $expect, $expectException = false)
+    {
+        if ($expectException) {
+            $this->setExpectedException(\RuntimeException::class);
+        }
+        $this->assertSame($expect, $this->config->getRequired($key));
+    }
+
+    public function providerGetRequired()
+    {
+        return [
+            ["string", static::$values["string"], false],
+            ["nope", "", true],
+        ];
     }
 }
