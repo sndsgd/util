2ea7699c2d2f991b922b60184181ac9257ff5fb7
Russell <r@snds.gd>
2017-03-18 20:37:17 -0400

    simplify container getter

--- tests/mock/ContainerMock.php
+++ tests/mock/ContainerMock.php
@@ -4,14 +4,14 @@ class ContainerMock extends \sndsgd\Container
 {
     public function getConfig(): \sndsgd\Config
     {
-        return $this->getSingleton("config", function() {
+        return $this->get("config", function() {
             return new \sndsgd\Config([]);
         });
     }
 
     public function resetConfig()
     {
-        $this->resetSingleton("config", function($name, $instance) {
+        $this->reset("config", function($name, $instance) {
             echo "$name ".get_class($instance);
         });
     }
